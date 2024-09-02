28483e8a2a5e5bb03d0d2af09f0cf7f5946e4e44
SebastianAlexis <sebastianralexis@gmail.com>
2024-09-01 20:38:31 -0700

    Updated ProbTwo to include questions

--- src/chapter1/ProbTwo.java
+++ src/chapter1/ProbTwo.java
@@ -1,5 +1,12 @@
 package chapter1;
 
+/*
+Suppose that we create an array A of GameEntry objects, which has an integer
+scores field, and we clone A and store the result in an array B. If we then im-
+mediately set A[4].score equal to 550, what is the score value of the GameEntry
+object referenced by B[4]?
+ */
+
 public class ProbTwo {
     private Integer score;
     public Integer getScore() {
